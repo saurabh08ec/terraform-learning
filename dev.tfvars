6caed6e201adc197e8bead552abc1684c8dd0407
saurabh08ec <adwicorp+catalyst@gmail.com>
2024-11-23 22:27:23 +0800

    updated output.tf and dev.tfvars file

--- dev.tfvars
+++ dev.tfvars
@@ -1,3 +1,3 @@
 aws_region    = "us-east-1"
 ami_id        = "ami-0583d8c7a9c35822c"
-instance_type = "t2.large"
+instance_type = "t2.medium"
--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "EC2_arn" {
   description = "The ARN of the EC2 instance"
   value       = aws_instance.mywebserver.arn
 }
+
+output "cbnode2" {
+  description = "The ARN of the EC2 instance"
+  value       = aws_instance.cbnode2.arn
+}
+
+output "cbnode3" {
+  description = "The ARN of the EC2 instance"
+  value       = aws_instance.cbnode3.arn
+}
